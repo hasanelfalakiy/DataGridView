05f618fbd1ce74e9f1c05ba3a768a7b9da946128
JKAS <jkas.dbt@gmail.com>
2024-10-25 10:43:50 +0200

    update and reorganization of codes.

--- app/src/main/java/jkas/datagridviewsample/MainActivity.java
+++ app/src/main/java/jkas/datagridviewsample/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.elevation.SurfaceColors;
 import java.util.ArrayList;
+import java.util.List;
 import jkas.datagridview.DataGridView;
 import jkas.datagridview.Row;
 import jkas.datagridviewsample.databinding.ActivityMainBinding;
@@ -20,26 +21,26 @@ public class MainActivity extends AppCompatActivity {
             // first you need to predefine your default settings for the grid
             DataGridView.DefaultSetting defSetting = binding.dataGridView.getDefaultSetting();
             defSetting.usePattern = true;
-            defSetting.listColorColumn.add(SurfaceColors.SURFACE_0.getColor(this));
-            defSetting.listColorColumn.add(SurfaceColors.SURFACE_4.getColor(this));
+            defSetting.addColorInListColumn(SurfaceColors.SURFACE_0.getColor(this));
+            defSetting.addColorInListColumn(SurfaceColors.SURFACE_4.getColor(this));
             defSetting.columnCount = 5; // or list.size() for your cellCounter
             // defSetting.@attr = values
 
             // depending on the size (maximum number) of your columns, create a data list
             // For example
             String[] dataRowHeader =
-                    new String[] {"header1", "header2", "header3", "header4", "header5"};
+                    new String[] {"Identification", "Name", "Home address", "Phone number", "Email"};
             String[] dataRow1 = new String[] {"cell1", "cell2", "cell3", "cell4", "cell5"};
             String[] dataRow2 = new String[] {"cell1", "cell2", "cell3", "cell4", "cell5"};
             String[] dataRow3 = new String[] {"cell1", "cell2", "cell3", "cell4", "cell5"};
-
+            
             // This list represents the data that will be in the grid.
-            ArrayList<String[]> listDATA = new ArrayList<>();
+            List<String[]> listDATA = new ArrayList<>();
             listDATA.add(dataRow1);
             listDATA.add(dataRow2);
             listDATA.add(dataRow3);
 
-            // ajout de l'entête
+            // adding header
             Row rowHeader =
                     binding.dataGridView.getRowHeader(); // new Row for Header will be created
             for (String dataCell : dataRowHeader) { // add column
@@ -56,24 +57,6 @@ public class MainActivity extends AppCompatActivity {
                 binding.dataGridView.addRow(row);
             }
         }
-        /*
-
-
-
-
-
-        */
-
-        /* // For the header of the data grid.
-        {
-            Row rowHeader = binding.dataGridView.getRowHeader();
-            rowHeader.addCell(rowHeader.newCell().setTextContent("Identification"));
-            rowHeader.addCell(rowHeader.newCell().setTextContent("Name"));
-            rowHeader.addCell(rowHeader.newCell().setTextContent("Home address"));
-            rowHeader.addCell(rowHeader.newCell().setTextContent("Phone number"));
-            rowHeader.addCell(rowHeader.newCell().setTextContent("Email"));
-            binding.dataGridView.setHeader(rowHeader);
-        }*/
 
         // for basic adding data.
         {
@@ -130,8 +113,8 @@ public class MainActivity extends AppCompatActivity {
         {
             DataGridView.DefaultSetting defSetting = binding.dataGridView2.getDefaultSetting();
             defSetting.usePattern = true;
-            defSetting.listColorRow.add(SurfaceColors.SURFACE_0.getColor(this));
-            defSetting.listColorRow.add(SurfaceColors.SURFACE_4.getColor(this));
+            defSetting.addColorInListColumn(SurfaceColors.SURFACE_0.getColor(this));
+            defSetting.addColorInListColumn(SurfaceColors.SURFACE_4.getColor(this));
             Row rowHeader = binding.dataGridView2.getRowHeader();
             rowHeader.addCell(rowHeader.newCell().setTextContent("Identification"));
             rowHeader.addCell(rowHeader.newCell().setTextContent("Name"));
