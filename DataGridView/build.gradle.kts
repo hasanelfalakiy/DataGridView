e4b9ddec8c55d5bc07be25332987dc9f71e2594f
andihasan97 <andihasanelfalakiy@gmail.com>
2024-11-04 01:09:58 +0000

    fix: build

--- DataGridView/build.gradle.kts
+++ DataGridView/build.gradle.kts
@@ -3,6 +3,28 @@ plugins {
     id("maven-publish")
 }
 
+android {
+    namespace = "jkas.datagridview"
+    compileSdk = 34
+    buildToolsVersion = "34.0.4"
+    
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+        
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            }
+        }
+    }
+}
+
+dependencies {
+    // No dependencies needed
+}
+
 afterEvaluate {
     publishing {
         publications {
@@ -15,19 +37,4 @@ afterEvaluate {
             }
         }
     }
-}
-
-android {
-    namespace = "jkas.datagridview"
-    compileSdk = 34
-    buildToolsVersion = "34.0.4"
-    
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 34
-    }
-}
-
-dependencies {
-    // No dependencies needed
 }
